245ce41564d84e8d62905b210da43d8b9a1fbf90
luffidwi12 <lufgandwi@gmail.com>
2022-01-25 15:19:50 +0700

    Belajat MVC

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php  
+
+class User_model {
+	private $nama ='Muhamad Luffi Dwi Daliana';
+
+	public function getUser()
+	{
+		return $this->nama;
+	}
+}
---
+++ app/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Halaman <?= $data['judul']?> </title>
+	<link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container">
+		<a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAlMarkup"  aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAlMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
+				<a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
+			</div>
+		</div>
+	</div>
+</nav>
